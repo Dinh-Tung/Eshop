65e5ca25f2b4bb2a6c47ed1db1238eab0c5638d2
thanh tung <meomeo1998x@gmail.com>
2020-05-09 16:00:12 +0700

    Complete configuration

--- EShopSolution.Data/Entities/Transaction.cs
+++ EShopSolution.Data/Entities/Transaction.cs
@@ -3,19 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespaceEeShopSolution.Data.Entities
+namespace EShopSolution.Data.Entities
 {
     public class Transaction
     {
         public int Id { set; get; }
         public DateTime TransactionDate { set; get; }
-        public int ExternalTransactionId { set; get; }
+        public string ExternalTransactionId { set; get; }
         public decimal Amount { set; get; }
         public decimal Fee { set; get; }
         public string Result { set; get; }
         public string Message { set; get; }
         public TransactionStatus Status { set; get; }
-        public int Provider { set; get; }
+        public string Provider { set; get; }
 
     }
 }
